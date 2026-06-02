948a49e5b9e529f8d593a6cf42de052fb530858c
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2026-06-02 22:27:53 +0530

    chore(nvim): re-enable `refactoring`
    Since, lazyvim update has arrived

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -43,6 +43,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.ui.treesitter-context" },
 
 		-- Editor Extras
+		{ import = "lazyvim.plugins.extras.editor.refactoring" },
 
 		-- import/override with your plugins
 		{ import = "plugins" },
