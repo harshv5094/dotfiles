9e1faac9e14767effcc2e926fe58acab8223c03f
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2026-09-19 23:09:07 +0530

    feat(nvim/snacks): add select session for dashboard
    Gives me quick access to my sessions

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -36,7 +36,13 @@ return {
 							desc = "Config",
 							action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
 						},
-						{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+						{ icon = "", key = "s", desc = "Restore Session", section = "session" },
+						{
+							icon = " ",
+							key = "S",
+							desc = "Select Session",
+							action = ':lua require("persistence").select()',
+						},
 						{ icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
 						{
 							icon = " ",
