b33a1a47afb209e7c9971b82a81ff72ee20070bf
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2026-09-22 19:30:34 +0530

    chore(blink.cmp): remove emoji support and turn off cmdline completion I'm not using it, so I'm removing it.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,19 +2,9 @@ return {
 	-- NOTE: blink.cmp - Code completion made easy
 	{
 		"saghen/blink.cmp",
-		dependencies = {
-			"moyiz/blink-emoji.nvim",
-		},
 		opts = {
-			sources = {
-				providers = {
-					emoji = {
-						module = "blink-emoji",
-						name = "Emoji",
-						score_offset = 15, -- Tune by preference
-						opts = { insert = true }, -- Insert emoji (default) or complete its name
-					},
-				},
+			cmdline = {
+				enabled = false,
 			},
 		},
 	},
