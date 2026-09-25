ec1702fdab40b2f6b845e691165260f8ba9aef49
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2026-09-25 18:29:48 +0530

    chore(nvim/which-key): replace deprecated which key function
    I'm replacing `register` with `add`

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -79,7 +79,7 @@ return {
 			wk.setup(opts)
 			if not vim.tbl_isempty(opts.defaults) then
 				LazyVim.warn("which-key: opts.defaults is deprecated. Please use opts.spec instead.")
-				wk.register(opts.defaults)
+				wk.add(opts.defaults)
 			end
 		end,
 	},
